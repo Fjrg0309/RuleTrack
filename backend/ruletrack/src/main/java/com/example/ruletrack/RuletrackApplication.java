424f66e7e20ed173ed8164a17b3200dc2ded70c3
Fran Redondo <franredondogonz@gmail.com>
2026-04-12 23:12:38 +0200

    Proyecto Springboot y Angular creados

---
+++ backend/ruletrack/src/main/java/com/example/ruletrack/RuletrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ruletrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuletrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuletrackApplication.class, args);
+	}
+
+}
---
+++ backend/ruletrack/src/test/java/com/example/ruletrack/RuletrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ruletrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuletrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
